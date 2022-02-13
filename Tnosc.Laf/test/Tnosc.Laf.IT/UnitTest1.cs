4f79b315763a6c5ecd0b6b2cc4ec4962fd382b82
Ahmed HEDFI <ahmed.hedfi@gmail.com>
2022-02-13 08:34:51 +0100

    Init

---
+++ Tnosc.Laf/test/Tnosc.Laf.IT/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Tnosc.Laf.IT
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Tnosc.Laf/test/Tnosc.Laf.UT/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Tnosc.Laf.UT
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
